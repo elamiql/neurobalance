ad85716a9820e4ff5415e87edb3ec5232ba2cd41
Agustín Salgado <agsalgado2023@udec.cl>
2026-03-31 23:03:49 -0300

    feat: added carrousel images, home reestructuration and added information about activities, objectives and mision/vision, etc

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
 import { NavegacionService } from '../../navegacion';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './home.html',
   styleUrl: './home.css'
 })
-export class Home {
-  constructor(public navService: NavegacionService) {}
+export class Home implements OnInit{
+  private http = inject(HttpClient);
+  public navService = inject(NavegacionService);
+
+  datosOng = signal<any>(null);
+
+slidesCarrusel = signal([
+    {
+      imagen: 'fotos/foto1.jpg',
+    },
+    {
+      imagen: 'fotos/foto2.jpg',
+    }
+  ]);
+
+  currentSlide = signal(0);
+
+  nextSlide() {
+    this.currentSlide.update(current => current === this.slidesCarrusel().length - 1 ? 0 : current + 1);
+  }
+
+  prevSlide() {
+    this.currentSlide.update(current => current === 0 ? this.slidesCarrusel().length - 1 : current - 1);
+  }
+  ngOnInit(): void {
+    this.http.get('/datos-ong.json').subscribe(res => {this.datosOng.set(res)});
+  }
 }
