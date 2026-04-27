347942692f14ca38d5203ce159fd6ba273bd3035
Agustín Salgado <agsalgado2023@udec.cl>
2026-04-27 17:58:45 -0400

    feat: added page of objectives and cards for them

--- src/app/pages/objetivos/objetivos.ts
+++ src/app/pages/objetivos/objetivos.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-objetivos',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './objetivos.html',
   styleUrl: './objetivos.css',
 })
-export class Objetivos {}
+export class Objetivos implements OnInit {
+  private http = inject(HttpClient);
+  datosOng = signal<any>(null);
+
+  ngOnInit(): void {
+    this.http.get('/datos-ong.json').subscribe(res => this.datosOng.set(res));
+  }
+}
