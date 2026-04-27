d3e5c2c8879fb4cbd46ee615b3cd65805655be2e
Agustín Salgado <agsalgado2023@udec.cl>
2026-04-27 18:27:02 -0400

    feat: added router on footer, function navegar unused and deleted

--- src/app/shared/footer/footer.ts
+++ src/app/shared/footer/footer.ts
@@ -1,27 +1,20 @@
 import { Component, OnInit, inject, signal } from '@angular/core';
-import { NavegacionService } from '../../navegacion';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
 export class Footer implements OnInit {
   private http = inject(HttpClient);
-  public navService = inject(NavegacionService);
-
   datosOng = signal<any>(null);
 
   ngOnInit(): void {
     this.http.get('/datos-ong.json').subscribe(res => this.datosOng.set(res));
-  }
-
-  navegar(seccion: string) {
-    this.navService.cambiarSeccion(seccion);
-    window.scrollTo({ top: 0, behavior: 'smooth' });
   }
 }
